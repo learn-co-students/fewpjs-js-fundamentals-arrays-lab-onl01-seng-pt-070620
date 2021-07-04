26f5bfb3569aa7461661c68003ac835af5894f30
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-07-04 07:24:10 +0430

    correct solution for prepend and remove element from array

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const pets = ["Milo", "Otis", "Garfield"];
 
 const append = [...pets, "Odie"]
+const prepend = ["Odie", ...pets]
+const removeLast = pets.slice(0,pets.length-1)
+const removeFirst = pets.slice(1)
